09858faa03e4443825275450572b786b4b0272eb
nidhi-1111 <nidhibodar17@gmail.com>
2024-04-24 16:00:58 -0400

    added dynamic input for websites

--- groq/app.py
+++ groq/app.py
@@ -19,44 +19,63 @@ load_dotenv()
 groq_api_key = os.environ['GROQ_API_KEY']
 google_api_key = os.environ['GOOGLE_API_KEY']
 
+st.title("Chat with Groq and Mixtral")
+website_link = st.text_input("Enter the website link:")
 
-if "vector" not in st.session_state:
+# if "vector" not in st.session_state:
+if website_link:
     # st.session_state.embeddings = OllamaEmbeddings()
     st.session_state.embeddings =GoogleGenerativeAIEmbeddings(model = 'models/embedding-001',google_api_key=google_api_key)
-    st.session_state.loader = WebBaseLoader("https://en.wikipedia.org/wiki/Jordan_Peterson")
+    st.session_state.loader = WebBaseLoader(website_link)
     st.session_state.docs = st.session_state.loader.load()
 
     st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size =1000, chunk_overlap= 200)
     st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
     st.session_state.vector = FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+    llm = ChatGroq(groq_api_key=groq_api_key, model="mixtral-8x7b-32768")
+    # mixtral-8x7b-32768
+    prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the question based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    </context>
+    Questions:{input}
+    """
+    )
+    document_chain = create_stuff_documents_chain(llm,prompt)
+    retriever = st.session_state.vector.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever,document_chain)
 
-st.title("Chat with Groq and Mixtral")
-llm = ChatGroq(groq_api_key=groq_api_key, model="mixtral-8x7b-32768")
-# mixtral-8x7b-32768
-prompt = ChatPromptTemplate.from_template(
-"""
-Answer the question based on the provided context only.
-Please provide the most accurate response based on the question
-<context>
-{context}
-</context>
-Questions:{input}
-"""
-)
-document_chain = create_stuff_documents_chain(llm,prompt)
-retriever = st.session_state.vector.as_retriever()
-retrieval_chain = create_retrieval_chain(retriever,document_chain)
+    prompt = st.text_input("Input your prompt here")
 
-prompt =st.text_input("Input your prompt here")
+# st.title("Chat with Groq and Mixtral")
+    if prompt:
+        # llm = ChatGroq(groq_api_key=groq_api_key, model="mixtral-8x7b-32768")
+        # # mixtral-8x7b-32768
+        # prompt = ChatPromptTemplate.from_template(
+        # """
+        # Answer the question based on the provided context only.
+        # Please provide the most accurate response based on the question
+        # <context>
+        # {context}
+        # </context>
+        # Questions:{input}
+        # """
+        # )
+        # document_chain = create_stuff_documents_chain(llm,prompt)
+        # retriever = st.session_state.vector.as_retriever()
+        # retrieval_chain = create_retrieval_chain(retriever,document_chain)
 
-if prompt:
-    start =time.process_time()
-    response = retrieval_chain.invoke({"input":prompt})
-    print("Response time :", time.process_time()-start)
-    st.write(response['answer'])
+        # if prompt:
+        start =time.process_time()
+        response = retrieval_chain.invoke({"input":prompt})
+        print("Response time :", time.process_time()-start)
+        st.write(response['answer'])
 
-    with st.expander("Document Similarity Search"):
-        for i, doc in enumerate(response['context']):
-            st.write(doc.page_content)
-            st.write("-----------------------------")
+        with st.expander("Document Similarity Search"):
+            for i, doc in enumerate(response['context']):
+                st.write(doc.page_content)
+                st.write("-----------------------------")
 
